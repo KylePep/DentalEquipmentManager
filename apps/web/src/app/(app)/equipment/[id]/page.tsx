e2a6f99dcaa693c99c3a44c2d7c5bda70dde847d
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2026-09-03 19:29:15 -0600

    temp image added to equipment details page

--- apps/web/src/app/(app)/equipment/[id]/page.tsx
+++ apps/web/src/app/(app)/equipment/[id]/page.tsx
@@ -1,13 +1,13 @@
 import { Heading } from "@/components/Heading";
 import { PageWrapper } from "@/components/PageWrapper";
+import Image from "next/image";
 
 export default function equipmentDetailPage() {
   return (
     <PageWrapper>
       <Heading level={1}>Equipment Detail</Heading>
-      <span>XYZ banana boat</span>
-      <span>fresh baked sourdough</span>
-      <p>Fish sticks are made of fish!</p>
+      <Heading level={2}>Dental chair</Heading>
+      <Image className="w-1/2 rounded-lg border-4 border-stone-700 shadow shadow-stone-900" src={"https://www.fsroson.com/wp-content/uploads/2024/08/FS06-Olive-green.jpg"} alt="dental chair" width={600} height={400}></Image>
     </PageWrapper>
   )
 }
