fa373a0dc452730d9a0b58a06cf5079f904a0f1e
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2026-09-03 18:19:19 -0600

    SideMenu open and closes

--- apps/web/src/components/SideMenu.tsx
+++ apps/web/src/components/SideMenu.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import type { Route } from "next";
 import { NavLink } from "./NavLink";
+import { useState } from "react";
 
 type NavItem = { href: Route; label: string; className: string };
 
@@ -9,18 +12,30 @@ const primary: NavItem[] = [
 ];
 
 const secondary: NavItem[] = [
-  { href: "/settings", label: "Settings", className: "border-stone-600 hover:border-stone-950" },
+  { href: "/settings", label: "Settings", className: "border-stone-600 hover:border-stone-950 w-full" },
 ];
 
 export function SideMenu() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="h-full flex flex-col justify-between bg-stone-900 w-32 py-4 px-2">
-      <section className="flex flex-col gap-2">
-        {primary.map((i) => <NavLink key={i.href} {...i} />)}
-      </section>
-      <section className="flex flex-col">
-        {secondary.map((i) => <NavLink key={i.href} {...i} />)}
-      </section>
-    </div>
+    <>
+
+      <div className={`h-full flex flex-col gap-4 justify-end items-center bg-stone-900 duration-300 transition-all py-4 px-2 overflow-hidden ${open ? 'min-w-42' : 'w-12'}`}>
+        {open && (
+          <section className="flex flex-col gap-2 w-full">
+            {primary.map((i) => <NavLink key={i.href} {...i} />)}
+          </section>
+        )}
+        {open && (
+          <section className="flex flex-col flex-grow gap-4 w-full items-center justify-end">
+            {secondary.map((i) => <NavLink key={i.href} {...i} />)}
+          </section>
+        )}
+        <button onClick={() => setOpen((open) => !open)} className="bg-stone-900 w-6 h-6 rounded flex items-center justify-center shadow shadow-black/90">
+          {open ? "X" : "O"}
+        </button>
+      </div>
+    </>
   );
 }
